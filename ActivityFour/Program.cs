3dbbfc26e2cfed31c12ff4d9089f4af8532c9d68
mardanniel <mginturo@you-source.com>
2023-03-07 15:07:54 +0800

    Add ActivityFour

---
+++ ActivityFour/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace ActivityFour
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = @"File Path";
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            StreamReader rd = new StreamReader(fs, Encoding.UTF8);
+            string strFromFile = rd.ReadToEnd();
+            string[] wordsList = strFromFile.Split(
+                new string[] {" ", Environment.NewLine}, 
+                StringSplitOptions.None
+                );
+
+            int evenLengthWords = 0;
+            foreach (string str in wordsList)
+            {
+                if (str.Length % 2 == 0)
+                {
+                    
+                    evenLengthWords++;
+                }
+            }
+
+            Console.WriteLine($"String from file: ");
+            Console.WriteLine(strFromFile);
+            Console.WriteLine($"Number of even length words: {evenLengthWords}");
+
+            rd.Close();
+            fs.Close();
+        }
+    }
+}
