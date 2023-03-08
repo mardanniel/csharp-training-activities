98de75a7ad17c39536cd6449df86c8136a0f5b12
mardanniel <mginturo@you-source.com>
2023-03-08 16:51:20 +0800

    Add additional task

--- ActivityFive/Program.cs
+++ ActivityFive/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ActivityFive
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace ActivityFive
 {
     class Program
     {
@@ -22,6 +24,7 @@
             // TaskThree();
             // TaskFour();
             // TaskFive();
+            // TaskSixSevenEight();
         }
 
         static void TaskOne()
@@ -90,5 +93,45 @@
             Console.WriteLine($"Even numbers from 1 to {max}:");
             PrintEvenNumbers(max);
         }
+
+        static void TaskSixSevenEight()
+        {
+            // List of employees
+            List<Employee> employees = new List<Employee>();
+            employees.Add(new Employee { Id = 1, Name = "Test One", Age = 18, Department = "IT", Salary = 10000 });
+            employees.Add(new Employee { Id = 2, Name = "Test Two", Age = 21, Department = "Admin", Salary = 9000 });
+            employees.Add(new Employee { Id = 3, Name = "Test Three", Age = 23, Department = "Marketing", Salary = 20000 });
+            employees.Add(new Employee { Id = 4, Name = "Test Four", Age = 18, Department = "IT", Salary = 8000 });
+            employees.Add(new Employee { Id = 5, Name = "Test Five", Age = 25, Department = "Admin", Salary = 25000 });
+            employees.FindAll((employee) => employee.Department == "Admin").ForEach((emp) => Console.WriteLine(emp.Name));
+
+            // Find and print all the employees that belong to a particular department
+            Func<string, List<Employee>> EmployeesByDepart = dep => employees.FindAll((employee) =>  employee.Department == dep);
+            string toFind = "Admin";
+            var taskSeven = EmployeesByDepart(toFind);
+            Console.WriteLine($"Employees from {toFind}:");
+            foreach(Employee employee in taskSeven)
+            {
+                Console.WriteLine(employee.Name);
+            }
+
+            // Find and print all the employees whose salary is less than 10000
+            Func<List<Employee>> EmployeesWithSalaryBelowTenThousand = () => employees.FindAll((employee) => employee.Salary < 10000);
+            var taskEight = EmployeesWithSalaryBelowTenThousand();
+            Console.WriteLine($"Employees whose salary is less than 10000:");
+            foreach (Employee employee in taskEight)
+            {
+                Console.WriteLine(employee.Name);
+            }
+        }
+    }
+
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
     }
 }
