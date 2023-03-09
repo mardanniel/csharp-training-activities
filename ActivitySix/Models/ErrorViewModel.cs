94763dc93386a41135ae9a3466fd4fbf7d100175
mardanniel <mginturo@you-source.com>
2023-03-10 00:10:54 +0800

    Add ActivitySix

---
+++ ActivitySix/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ActivitySix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
