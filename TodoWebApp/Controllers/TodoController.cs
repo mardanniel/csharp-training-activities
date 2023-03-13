bf4d15d9cf8048e30cf966eee70d5ab3c2d76aa7
Mar Danniel Ginturo <60316252+mardanniel@users.noreply.github.com>
2023-03-13 05:58:11 +0000

    some changes

--- TodoWebApp/Controllers/TodoController.cs
+++ TodoWebApp/Controllers/TodoController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using TodoWebApp.Models;
 using TodoWebApp.Repository.InMemory;
+using TodoWebApp.ViewModels;
 
 namespace TodoWebApp.Controllers
 {
@@ -28,6 +29,27 @@ namespace TodoWebApp.Controllers
     {
       imr.DeleteTodoById(todoId);
       return RedirectToAction(controllerName: "Todo", actionName: "GetAllTodos");
+    }
+
+    public IActionResult Test(){
+
+      // 3 ways to pass data to view
+      // 1. ViewData
+      ViewData["Name"] = "Hotdog";
+      // 2. ViewBag
+      ViewBag.Something = "Hahaha";
+      // 3. Strongly Types - ex. Model
+      Todo todo = imr.GetTodoById(0);
+
+      //  ViewModel
+      SomeActionViewModel smvm = new SomeActionViewModel();
+      smvm.todo = todo;
+      smvm.user = "Something";
+
+
+      // return View(todo);
+
+      return View(smvm);
     }
   }
 }
---
+++ TodoWebApp/ViewModels/SomeActionViewModel.cs
@@ -0,0 +1,10 @@
+using TodoWebApp.Models;
+
+namespace TodoWebApp.ViewModels
+{
+  public class SomeActionViewModel
+  {
+    public Todo todo;
+    public string user;
+  }
+}
